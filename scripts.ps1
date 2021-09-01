37d5d85ffc86fa34d3008be86915705098fb4e8b
jmd <jamesmariodavis@gmail.com>
2021-08-31 20:04:02 -0700

    powershell

--- scripts.ps1
+++ scripts.ps1
@@ -1,42 +1,63 @@
 # set app name. change this, and only this, to localize to new app
 $APP_NAME="python_base"
 # get paramaeters passed by user
-$PASSED_PARAMETER=$args[0]
+$SCRIPT_ACTION_ARG=$args[0]
+$APP_NAME_ARG=$args[1]
 # retreive absolute path
 $ABSOLUTE_PATH=Get-Location
 # set image names relative to app name
 $DEV_IMAGE_NAME="${APP_NAME}_dev"
 $PROD_IMAGE_NAME="${APP_NAME}_prod"
+function Replace-Line-In-File {
+    param (
+        [string]$RegularExpression,
+        [string]$ReplacementString,
+        [string]$TargetFile
+    )
+    $Matches=Select-String -Pattern $RegularExpression -Path $TargetFile
+    [int]$MatchCount=$Matches.Count
+    $ErrorString='ERROR! Found {0} matches for: {1} file: {2}' -f $MatchCount, $RegularExpression.PadRight(30,' '), $TargetFile
+    if ($MatchCount -eq 0) {
+        Write-Output $ErrorString
+    }
+    elseif ($MatchCount -gt 1) {
+        Write-Output $ErrorString
+    }
+    else {
+        (Get-Content $TargetFile) -replace $RegularExpression, $ReplacementString | Set-Content $TargetFile
+        $SuccessString='replaced string: {0} file: {1}' -f $ReplacementString.PadRight(30,' '), $TargetFile
+        Write-Output $SuccessString
+    }
+}
 
-
-if ($PASSED_PARAMETER -eq 'build-dev-image'){
+if ($SCRIPT_ACTION_ARG -eq '--build-image-dev'){
     Invoke-Command -ScriptBlock {
         docker build `
         --tag ${DEV_IMAGE_NAME}:latest `
         --file docker/Dockerfile.dev .
     }
 }
-elseif ($PASSED_PARAMETER -eq 'build-prod-image') {
+elseif ($SCRIPT_ACTION_ARG -eq '--build-image-prod') {
     Invoke-Command -ScriptBlock {
         docker build `
         --tag ${PROD_IMAGE_NAME}:latest `
         --file docker/Dockerfile.prod .
     }
 }
-elseif ($PASSED_PARAMETER -eq "enter-dev-container") {
+elseif ($SCRIPT_ACTION_ARG -eq "--enter-container-dev") {
     Invoke-Command -ScriptBlock {
         docker run -it `
         --entrypoint="" `
         --rm `
         --net=host `
-        --workdir=/${APP_NAME} `
-	    --env PYTHONPATH=${APP_NAME} `
-        --volume ${ABSOLUTE_PATH}:/${APP_NAME} `
+        --workdir=/app `
+	    --env PYTHONPATH=/app `
+        --volume ${ABSOLUTE_PATH}:/app `
         ${DEV_IMAGE_NAME}:latest `
         /bin/bash
     }
 }
-elseif ($PASSED_PARAMETER -eq "enter-prod-container") {
+elseif ($SCRIPT_ACTION_ARG -eq "--enter-container-prod") {
     Invoke-Command -ScriptBlock {
         docker run -it `
         --entrypoint="" `
@@ -47,6 +68,31 @@ elseif ($PASSED_PARAMETER -eq "enter-prod-container") {
         /bin/bash
     }
 }
+elseif ($SCRIPT_ACTION_ARG -eq "--set-app-name") {
+    if (!$APP_NAME_ARG) {
+        Write-Output "must pass app name as second arg"
+        exit 1
+    }
+    $RegularExpression="APP_NAME[ ]*=[ ]*`".*`""
+    $ReplacementString="APP_NAME=`"${APP_NAME_ARG}`""
+    $TargetFile="scripts.ps1"
+    Replace-Line-In-File -RegularExpression $RegularExpression -ReplacementString $ReplacementString -TargetFile $TargetFile
+
+    $RegularExpression="^APP_NAME[ ]*=.*"
+    $ReplacementString="APP_NAME=${APP_NAME_ARG}"
+    $TargetFile="scripts.sh"
+    Replace-Line-In-File -RegularExpression $RegularExpression -ReplacementString $ReplacementString -TargetFile $TargetFile
+
+    $RegularExpression="^name[ ]*=[ ]*`".*`""
+    $ReplacementString="name = `"${APP_NAME_ARG}`""
+    $TargetFile="pyproject.toml"
+    Replace-Line-In-File -RegularExpression $RegularExpression -ReplacementString $ReplacementString -TargetFile $TargetFile
+
+    $RegularExpression="`"image`":[ ]*`".*`"[ ]*,"
+    $ReplacementString="`"image`": `"${APP_NAME_ARG}_dev:latest`","
+    $TargetFile=".devcontainer/devcontainer.json"
+    Replace-Line-In-File -RegularExpression $RegularExpression -ReplacementString $ReplacementString -TargetFile $TargetFile
+}
 else {
-    Write-Output "option ${PASSED_PARAMETER} not found"
+    Write-Output "action ${SCRIPT_ACTION_ARG} not found"
 }
