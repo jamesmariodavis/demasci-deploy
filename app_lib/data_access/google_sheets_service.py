298589218e3676d13d9d9c0b3e512fb0cad46b46
jdavis <jdavis@flexport.com>
2021-09-11 22:34:57 +0000

    gsheets

--- app_lib/data_access/google_sheets_service.py
+++ app_lib/data_access/google_sheets_service.py
@@ -1,43 +1,181 @@
-from typing import List, Any
+from typing import (
+    List,
+    Any,
+)
+from enum import Enum
 from googleapiclient.discovery import build
+import numpy as np
 import pandas as pd
 
-MAX_CELL_RANGE = 'A1:ZZ999999'
+
+class ValueInputOption(Enum):
+    RAW = 'RAW'
+    USER_ENTERED = 'USER_ENTERED'
+
+
+class MajorDimension(Enum):
+    ROWS = 'ROWS'  # default
+    COLUMNS = 'COLUMNS'
+
+
+class CellData(Enum):
+    # https://developers.google.com/sheets/api/reference/rest/v4/spreadsheets/cells#CellData
+    USER_ENTERED_VALUE = 'userEnteredValue'
+    USER_ENTERED_FORMAT = 'userEnteredFormat'
+
+
+def _get_sheet_service() -> Any:
+    # pylint: disable=no-member
+    service = build('sheets', 'v4')
+    sheet = service.spreadsheets()  # type: ignore
+    return sheet
+
+
+def _get_sheet_name_and_range(
+    sheet_name: str,
+    cell_range: str,
+) -> str:
+    if cell_range:
+        sheet_name_and_range = '{}!{}'.format(sheet_name, cell_range)
+    else:
+        sheet_name_and_range = sheet_name
+    return sheet_name_and_range
 
 
 class GoogleSheetsService:
-    def __init__(self) -> None:
-        # pylint: disable=no-member
-        service = build('sheets', 'v4')
-        self.sheet = service.spreadsheets()  # type: ignore
+    @staticmethod
+    def get_sheets_values_clear(spreadsheet_id: str):
+        return GoogleSheetsValuesClear(spreadsheet_id=spreadsheet_id)
 
-    def get_values_from_range(
-        self,
-        spreadsheet_id: str,
-        sheet_name: str,
-        cell_range: str,
-    ) -> List[List[Any]]:
-        sheet_name_and_range = '{}!{}'.format(sheet_name, cell_range)
-        result = self.sheet.values().get(
-            spreadsheetId=spreadsheet_id,
-            range=sheet_name_and_range,
-        ).execute()
-        values = result.get('values', [])
-        return values
+    @staticmethod
+    def get_sheets_values_updater(spreadsheet_id: str):
+        return GoogleSheetsValuesUpdater(spreadsheet_id=spreadsheet_id)
 
+    @staticmethod
+    def get_sheets_values_getter(spreadsheet_id: str):
+        return GoogleSheetsValuesGetter(spreadsheet_id=spreadsheet_id)
+
+    @staticmethod
     def get_sheet_as_frame(
-        self,
         spreadsheet_id: str,
         sheet_name: str,
         first_row_is_column_names: bool = False,
     ) -> Any:
-        values = self.get_values_from_range(
-            spreadsheet_id=spreadsheet_id,
+        sheets_values_getter = GoogleSheetsValuesGetter(spreadsheet_id=spreadsheet_id)
+        sheets_values_getter.add_range(
             sheet_name=sheet_name,
-            cell_range=MAX_CELL_RANGE,
+            cell_range='',
         )
+        result = sheets_values_getter.execute()
+        values = result[0]
         frame = pd.DataFrame(values)
+        frame = frame.replace('', np.nan).fillna(np.nan)
         if first_row_is_column_names:
             frame.columns = frame.iloc[0]
             frame = frame[1:].reset_index(drop=True)
         return frame
+
+    @staticmethod
+    def write_frame_to_sheet(
+        frame: pd.DataFrame,
+        spreadsheet_id: str,
+        sheet_name: str,
+        write_columns_names: bool = True,
+    ) -> Any:
+        sheets_values_updater = GoogleSheetsValuesUpdater(spreadsheet_id=spreadsheet_id)
+        list_of_rows = frame.values.tolist()
+        if write_columns_names:
+            list_of_rows.insert(0, list(frame.columns))
+        sheets_values_updater.add_data(
+            values=list_of_rows,
+            sheet_name=sheet_name,
+            cell_range='',
+        )
+        sheets_values_updater.execute()
+
+
+class GoogleSheetsValuesUpdater:
+    def __init__(self, spreadsheet_id: str) -> None:
+        self.spreadsheet_id = spreadsheet_id
+        self.data = []
+        self.sheet = _get_sheet_service()
+
+    def execute(self) -> None:
+        body = {
+            'valueInputOption': ValueInputOption.RAW.value,
+            'data': self.data,
+        }
+        _ = self.sheet.values().batchUpdate(
+            spreadsheetId=self.spreadsheet_id,
+            body=body,
+        ).execute()
+
+    def add_data(
+        self,
+        sheet_name: str,
+        cell_range: str,
+        values: List[List[Any]],
+    ) -> None:
+        sheet_name_and_cell_range = _get_sheet_name_and_range(
+            sheet_name=sheet_name,
+            cell_range=cell_range,
+        )
+        update_range = {
+            'values': values,
+            'range': sheet_name_and_cell_range,
+            'majorDimension': MajorDimension.ROWS.value,
+        }
+        self.data.append(update_range)
+
+
+class GoogleSheetsValuesClear:
+    def __init__(self, spreadsheet_id: str) -> None:
+        self.spreadsheet_id = spreadsheet_id
+        self.ranges = []
+        self.sheet = _get_sheet_service()
+
+    def execute(self) -> None:
+        body = {'ranges': self.ranges}
+        _ = self.sheet.values().batchClear(
+            spreadsheetId=self.spreadsheet_id,
+            body=body,
+        ).execute()
+
+    def add_clear_cell_data(
+        self,
+        sheet_name: str,
+        cell_range: str,
+    ) -> None:
+        sheet_name_and_range = _get_sheet_name_and_range(
+            sheet_name=sheet_name,
+            cell_range=cell_range,
+        )
+        self.ranges.append(sheet_name_and_range)
+
+
+class GoogleSheetsValuesGetter:
+    def __init__(self, spreadsheet_id: str) -> None:
+        self.spreadsheet_id = spreadsheet_id
+        self.range_names = []
+        self.sheet = _get_sheet_service()
+
+    def execute(self) -> None:
+        result = self.sheet.values().batchGet(
+            spreadsheetId=self.spreadsheet_id,
+            ranges=self.range_names,
+            majorDimension=MajorDimension.ROWS.value,
+        ).execute()
+        ranges = result.get('valueRanges', [])
+        values = [i.get('values', []) for i in ranges]
+        return values
+
+    def add_range(
+        self,
+        sheet_name: str,
+        cell_range: str,
+    ) -> None:
+        sheet_name_and_range = _get_sheet_name_and_range(
+            sheet_name=sheet_name,
+            cell_range=cell_range,
+        )
+        self.range_names.append(sheet_name_and_range)
--- tests/data_access/test_google_sheets.py
+++ tests/data_access/test_google_sheets.py
@@ -2,39 +2,95 @@ from typing import List, Any
 from unittest.mock import patch
 import pytest
 import pandas as pd
+import numpy as np
 from tests.base_test_case import BaseTestCase
-from app_lib.data_access.google_sheets_service import GoogleSheetsService
+from app_lib.data_access.google_sheets_service import (
+    GoogleSheetsService,
+    GoogleSheetsValuesGetter,
+)
 
+# test sheet: https://docs.google.com/spreadsheets/d/1Uxi1SXc8CkpZcMPPmMrN5Z-8VRZXc1u62LFQVwkuMhs
 TEST_SHEET_ID = '1Uxi1SXc8CkpZcMPPmMrN5Z-8VRZXc1u62LFQVwkuMhs'
 SHEET1 = 'Sheet1'
 SHEET2 = 'Sheet2'
 
-# test sheet: https://docs.google.com/spreadsheets/d/1Uxi1SXc8CkpZcMPPmMrN5Z-8VRZXc1u62LFQVwkuMhs
 
-
-def patch_get_values_from_range(
+def patch_values_getter_execute(
     # pylint: disable=unused-argument
-    self: GoogleSheetsService,
-    spreadsheet_id: str,
-    sheet_name: str,
-    cell_range: str,
-) -> List[List[Any]]:
+    self: GoogleSheetsValuesGetter, ) -> List[List[Any]]:
     # cell data is the values without header
-    values: List[List[Any]] = [['a', 'b'], ['1', '4'], [None, '0']]
+    values: List[List[Any]] = [
+        [['a', 'b'], ['4'], ['', '0']],
+    ]
     return values
 
 
 class TestGoogleSheets(BaseTestCase):
     @pytest.mark.external_deps
-    def test_get_values_from_range(self) -> None:
-        google_sheets_service = GoogleSheetsService()
-        returned_values = google_sheets_service.get_values_from_range(
-            spreadsheet_id=TEST_SHEET_ID,
+    def test_google_sheets_value_getter(self) -> None:
+        value_getter = GoogleSheetsService.get_sheets_values_getter(spreadsheet_id=TEST_SHEET_ID)
+        value_getter.add_range(
             sheet_name=SHEET1,
-            cell_range='A1:B3',
+            cell_range='',
         )
-        expected_values = [['a', 'b'], ['1'], ['2', '3']]
-        self.assertListEqual(returned_values, expected_values)
+        value_getter.add_range(
+            sheet_name=SHEET1,
+            cell_range='A1:B2',
+        )
+        returned_value = value_getter.execute()
+        expected_value = [
+            [
+                ['a', 'b'],
+                ['1'],
+                ['2', '3'],
+                ['', '7'],
+            ],
+            [
+                ['a', 'b'],
+                ['1'],
+            ],
+        ]
+        self.assertListEqual(returned_value, expected_value)
+
+    @pytest.mark.external_deps
+    def test_google_sheets_update_cycle(self) -> None:
+        value_to_write = [['1', '2'], ['a', '7']]
+        # write data to sheet
+        value_updater = GoogleSheetsService.get_sheets_values_updater(spreadsheet_id=TEST_SHEET_ID)
+        value_updater.add_data(
+            sheet_name=SHEET2,
+            cell_range='',
+            values=value_to_write,
+        )
+        value_updater.execute()
+
+        # verify data has been written
+        value_getter = GoogleSheetsService.get_sheets_values_getter(spreadsheet_id=TEST_SHEET_ID)
+        value_getter.add_range(
+            sheet_name=SHEET2,
+            cell_range='',
+        )
+        returned_value = value_getter.execute()
+        expected_value = [value_to_write]
+        self.assertListEqual(returned_value, expected_value)
+
+        # clear data
+        value_clear = GoogleSheetsService.get_sheets_values_clear(spreadsheet_id=TEST_SHEET_ID)
+        value_clear.add_clear_cell_data(
+            sheet_name=SHEET2,
+            cell_range='',
+        )
+        value_clear.execute()
+
+        # verify data has been cleared
+        value_getter = GoogleSheetsService.get_sheets_values_getter(spreadsheet_id=TEST_SHEET_ID)
+        value_getter.add_range(
+            sheet_name=SHEET2,
+            cell_range='',
+        )
+        returned_value = value_getter.execute()
+        expected_value = [[]]
+        self.assertListEqual(returned_value, expected_value)
 
     @pytest.mark.external_deps
     def test_get_sheet_as_frame(self) -> None:
@@ -45,8 +101,8 @@ class TestGoogleSheets(BaseTestCase):
             first_row_is_column_names=False,
         )
         expected_frame = pd.DataFrame({
-            0: ['a', '1', '2', ''],
-            1: ['b', None, '3', '7'],
+            0: ['a', '1', '2', np.nan],
+            1: ['b', np.nan, '3', '7'],
         })
         self.assertFramesEqual(returned_frame, expected_frame)
 
@@ -56,39 +112,65 @@ class TestGoogleSheets(BaseTestCase):
             first_row_is_column_names=True,
         )
         expected_frame = pd.DataFrame({
-            'a': ['1', '2', ''],
-            'b': [None, '3', '7'],
+            'a': ['1', '2', np.nan],
+            'b': [np.nan, '3', '7'],
         })
         self.assertFramesEqual(returned_frame, expected_frame)
 
     @patch.object(
-        target=GoogleSheetsService,
-        attribute='get_values_from_range',
-        new=patch_get_values_from_range,
+        target=GoogleSheetsValuesGetter,
+        attribute='execute',
+        new=patch_values_getter_execute,
     )
     def test_get_sheet_as_frame_local(self) -> None:
-        google_sheets_service = GoogleSheetsService()
-        returned_frame = google_sheets_service.get_sheet_as_frame(
-            spreadsheet_id=TEST_SHEET_ID,
-            sheet_name=SHEET1,
+        returned_frame = GoogleSheetsService.get_sheet_as_frame(
+            spreadsheet_id='junk',
+            sheet_name='junk',
             first_row_is_column_names=False,
         )
         expected_frame = pd.DataFrame({
-            0: ['a', '1', None],
-            1: ['b', '4', '0'],
+            0: ['a', '4', np.nan],
+            1: ['b', np.nan, '0'],
         })
         self.assertFramesEqual(returned_frame, expected_frame)
 
-        returned_frame = google_sheets_service.get_sheet_as_frame(
-            spreadsheet_id=TEST_SHEET_ID,
-            sheet_name=SHEET1,
+        returned_frame = GoogleSheetsService.get_sheet_as_frame(
+            spreadsheet_id='junk',
+            sheet_name='junk',
             first_row_is_column_names=True,
         )
         expected_frame = pd.DataFrame({
-            'a': ['1', None],
-            'b': ['4', '0'],
+            'a': ['4', np.nan],
+            'b': [np.nan, '0'],
         })
         self.assertFramesEqual(returned_frame, expected_frame)
+
+    @pytest.mark.external_deps
+    def test_write_frame_to_sheet(self) -> None:
+        frame_to_write = pd.DataFrame({
+            'a': ['1', '2'],
+            'b': ['3', '4'],
+        })
+        GoogleSheetsService.write_frame_to_sheet(
+            frame=frame_to_write,
+            spreadsheet_id=TEST_SHEET_ID,
+            sheet_name=SHEET2,
+            write_columns_names=True,
+        )
+        returned_frame = GoogleSheetsService.get_sheet_as_frame(
+            spreadsheet_id=TEST_SHEET_ID,
+            sheet_name=SHEET2,
+            first_row_is_column_names=True,
+        )
+        self.assertFramesEqual(frame_to_write, returned_frame)
+
+        # clear data
+        value_clear = GoogleSheetsService.get_sheets_values_clear(spreadsheet_id=TEST_SHEET_ID)
+        value_clear.add_clear_cell_data(
+            sheet_name=SHEET2,
+            cell_range='',
+        )
+        value_clear.execute()
 
 
 if __name__ == '__main__':
